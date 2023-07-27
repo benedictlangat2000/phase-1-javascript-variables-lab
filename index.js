86c2f86824f505fe8381892bc6c1f7de9eeb24f3
BenedictKipkuruiLangat <benedictlangat2000@gmail.com>
2023-07-27 23:12:43 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+const fs = require('fs')
+const path = require('path')
+const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+
+const companyName = 'Scuber';
+let mostProfitableNeighborhood='Chelsea';
+let companyCeo = 'Susan Smith'
