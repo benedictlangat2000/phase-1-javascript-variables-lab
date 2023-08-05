03974bf5052aec0d47e4557543db7c0db873f2cd
BenedictKipkuruiLangat <benedictlangat2000@gmail.com>
2023-08-05 17:28:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const fs = require('fs')
-const path = require('path')
-const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+
 
 const companyName = 'Scuber';
 let mostProfitableNeighborhood='Chelsea';
